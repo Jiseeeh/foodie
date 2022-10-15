8e4d41f4e35fd37da726469a0717da8a06605d1e
Jiseeeh <johncarlo.camara1@gmail.com>
2022-10-15 18:51:00 +0800

    feat: add menu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 import Navbar from "../components/Navbar";
+import Menu from "../components/Menu";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Navbar />
+      <Menu/>
       <Component {...pageProps} />
     </>
   );
