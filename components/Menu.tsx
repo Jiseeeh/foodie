44dfa1465ebd4ac205e9e1c83444ee6b1252e7c1
Jiseeeh <johncarlo.camara1@gmail.com>
2022-10-16 20:47:11 +0800

    feat: jpg to webp

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -6,21 +6,21 @@ import Button from "./Button";
 const Menu: React.FC = () => {
   const cards = [
     {
-      imgSrc: "/ramen.jpg",
+      imgSrc: "/ramen.webp",
       imgAlt: "Bowl of Ramen",
       cardTitle: "Ramen",
       cardDesc: "Lorem ipsum dolor sit amet, consectetur  adipisicing...",
       price: "$13",
     },
     {
-      imgSrc: "/ramen-1.jpg",
+      imgSrc: "/ramen-1.webp",
       imgAlt: "Bowl of Ramen",
       cardTitle: "Ramen",
       cardDesc: "Lorem ipsum dolor sit amet, consectetur  adipisicing...",
       price: "$13",
     },
     {
-      imgSrc: "/ramen-2.jpg",
+      imgSrc: "/ramen-2.webp",
       imgAlt: "Bowl of Ramen",
       cardTitle: "Ramen",
       cardDesc: "Lorem ipsum dolor sit amet, consectetur  adipisicing...",
@@ -33,7 +33,9 @@ const Menu: React.FC = () => {
       id="menu"
       className="flex flex-col items-center bg-dimmed-green md:-mt-40 lg:-mt-52 xl:-mt-60"
     >
-      <h1 className="mb-4 mt-16 font-bold text-3xl text-dimmed-blue">Popular Menu</h1>
+      <h1 className="mb-4 mt-16 font-bold text-3xl text-dimmed-blue">
+        Popular Menu
+      </h1>
       <section className="grid gap-5 md:grid-cols-3">
         {cards.map((card, index) => (
           <Card key={index} {...card} />
