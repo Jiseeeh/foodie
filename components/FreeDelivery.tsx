f2cb20b8e3f4e30e556303d12ba86163b146241b
Jiseeeh <johncarlo.camara1@gmail.com>
2022-12-02 21:08:23 +0800

    style: increase font size on md

--- components/FreeDelivery.tsx
+++ components/FreeDelivery.tsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 const FreeDelivery: React.FC = () => {
   return (
     <section className="p-3 min-h-screen text-dimmed-blue flex flex-col justify-center items-center bg-food-bg bg-cover">
-      <h1 className="mb-3 text-4xl font-bold">Free Delivery</h1>
+      <h1 className="mb-3 text-4xl font-bold md:text-5xl">Free Delivery</h1>
       <p className="md:max-w-xl md:text-xl">
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
         nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
